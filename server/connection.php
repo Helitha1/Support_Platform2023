50f47982c5e96bd45c5460d063b648997c9ad577
sasindu2002 <113811301+sasindu2002@users.noreply.github.com>
2023-10-05 12:52:44 +0530

    Update connection.php

--- server/connection.php
+++ server/connection.php
@@ -7,7 +7,7 @@ class Database{
     public static function setUpConnection(){
 
         if (!isset(Database::$connection)) {
-            Database::$connection = new mysqli("localhost", "root", "sasiMAX200@123", "migten1", "3306");
+            Database::$connection = new mysqli("localhost", "root", "sasiMAX200@123", "migten2", "3306");
         }
     }
 
