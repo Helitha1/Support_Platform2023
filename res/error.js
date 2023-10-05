bbc193a8b778a6d11c1012c7ac51c31d30011309
Yaso <yasogaran@gmail.com>
2023-10-05 12:34:10 +0530

    Update error.js

--- res/error.js
+++ res/error.js
@@ -9,7 +9,25 @@ showError=(code)=>{
         case 2:
             return('Last Name is NULL')
             break;
-        case 99:
+        case 3:
+            return('email')
+            break;
+        case 4:
+            return('REQUEST JSON ERROR')
+            break;
+        case 5:
+            return('REQUEST JSON ERROR')
+            break;
+        case 6:
+            return('REQUEST JSON ERROR')
+            break;
+        case 7:
+            return('REQUEST JSON ERROR')
+            break;
+        case 8:
+            return('REQUEST JSON ERROR')
+            break;
+        case 9:
             return('REQUEST JSON ERROR')
             break;
     
