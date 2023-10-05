0be1de879ca168869e90e9931e7d16e43dda3bd3
Yaso <yasogaran@gmail.com>
2023-10-05 15:16:50 +0530

    Update resetpassword.php

--- server/resetpassword.php
+++ server/resetpassword.php
@@ -10,27 +10,27 @@ if (isset($_POST["json"])) {
      $request_obj = json_decode($_POST["json"]);
      // CHECK JSON PARAMETERS
      if (isset($request_obj->email) && isset($request_obj->password) && isset($request_obj->password2) && isset($request_obj->vcode)) {
-          if ($request_obj->email=="") {
+          if ($request_obj->email == "") {
                $code = 12;
-          } elseif ($request_obj->password=="") {
+          } else if ($request_obj->password == "") {
                $code = 8;
-          } elseif (strlen($request_obj->password) < 5 || strlen($request_obj->password) > 20) {
+          } else if (strlen($request_obj->password) < 5 || strlen($request_obj->password) > 20) {
                $code = 9;
-          } elseif ($request_obj->password2=="") {
+          } else if ($request_obj->password2 == "") {
                $code = 14;
-          } elseif ($request_obj->password != $request_obj->password2) {
-               $code =15;
-          } else if ($request_obj->vcode=="") {
-               $code =16;
+          } else if ($request_obj->password != $request_obj->password2) {
+               $code = 15;
+          } else if ($request_obj->vcode == "") {
+               $code = 16;
           } else {
-// SEARCH USER FROM DATABASE
+               // SEARCH USER FROM DATABASE
                $rs =  Database::search("SELECT * FROM `users` WHERE `email`='" . $request_obj->email . "' AND
               `verification_code`= '" . $request_obj->vcode . "'");
-// CHECK USER IN DATABASE
+               // CHECK USER IN DATABASE
                if ($rs->num_rows == 1) {
                     // UPDATE NEW PASSWORD
                     Database::iud("UPDATE `users` SET `password`='" . $request_obj->password . "', `verification_code`='' WHERE `email`='" . $request_obj->email . "'");
-                    $code =100;
+                    $code = 100;
                } else {
                     $code = 17;
                }
