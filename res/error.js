283fdfb31b92ef2140ddfc60824057aac6f5d5d0
sasindu2002 <113811301+sasindu2002@users.noreply.github.com>
2023-10-05 12:41:40 +0530

    Update error.js

--- res/error.js
+++ res/error.js
@@ -4,31 +4,37 @@ showError=(code)=>{
             return('REQUEST JSON ERROR')
             break;
         case 1:
-            return('First Name is NULL')
+            return('First Name is null ')
             break;
         case 2:
-            return('Last Name is NULL')
+            return('Last Name is null')
             break;
         case 3:
-            return('email')
+            return('first name must be less that 45 characters ')
             break;
         case 4:
-            return('REQUEST JSON ERROR')
+            return('last name must be less that 45 characters ')
             break;
         case 5:
-            return('REQUEST JSON ERROR')
+            return('email is null')
             break;
         case 6:
-            return('REQUEST JSON ERROR')
+            return('email must be less that 100 characters ')
             break;
         case 7:
-            return('REQUEST JSON ERROR')
+            return('invalid email')
             break;
         case 8:
-            return('REQUEST JSON ERROR')
+            return('password is null')
             break;
         case 9:
-            return('REQUEST JSON ERROR')
+            return('password must be more that 5 and less that 20 characters')
+            break;
+        case 10:
+            return('profession not selected')
+            break;
+        case 11:
+            return('this email alredy used');
             break;
     
         default:
