1840fe6cf907ef6a92f5b2363ebfc3a9c2e33e7a
Nikeshana <146467081+Nikeshana@users.noreply.github.com>
2023-10-05 11:54:27 +0530

    Create about.php

---
+++ about.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>About Us</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+</head>
+<body style="background-color: #a3f4b9";>
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12 text-center">
+        <h1 class="mt-5">About Us</h1>
+        <p class="lead">
+Welcome to our Charity Donation Platform, where compassion meets action! At our core, we believe in the power of collective kindness to make a difference in the world.
+Our platform is more than just a space for transactions; it's a vibrant community united by the common goal of creating positive change. On this About page, we invite you to delve into the heart of who we are and what drives us.</p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-lg-6">
+        <h2>Our Mission</h2>
+        <p>We exist to bridge the gap between generosity and those in need. Our mission is simple yet profound—to amplify the impact of every act 
+            of kindness. Whether it's providing essential resources, supporting vital causes, or 
+            contributing to transformative projects, we aim to be the catalyst for meaningful change.</p>
+      </div>
+      <div class="row">
+      <div class="col-lg-12 text-center">
+        <h1 class="mt-5">The Power of Unity:</h1>
+        <p class="lead">
+        We firmly believe that real change happens when individuals come together. Our platform is a testament to the strength of unity, 
+        where every donation, regardless of size, plays a crucial role in building a better world. Here, you are not just a donor; 
+        you are an integral part of a global movement for positive change.</p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-lg-12 text-center">
+        <h1 class="mt-5">Transparency and Trust:</h1>
+        <p class="lead">
+        Integrity is at the core of our operations. We are committed to transparency in every aspect of our platform.
+         From where your donations go to the impact they create, we want you to have full confidence in the journey of your contribution.
+          Trust is the foundation of our community, and we strive to uphold it with utmost diligence.</p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-lg-12 text-center">
+        <h1 class="mt-5">Celebrating Impact:</h1>
+        <p class="lead">
+        Every success story, every life touched, and every community uplifted is a cause for celebration. On this platform,
+         we share the stories of impact that your contributions make possible. It's a space to rejoice in the collective difference
+          we are making and to inspire even greater acts of kindness.</p>
+      </div>
+    </div>
+      <div class="col-lg-12">
+        <h2>Our Team</h2>
+        <p>Behind the scenes, a dedicated team fuels the engine of our platform. Comprising individuals passionate about social change, 
+            technology, and philanthropy, we are united by a shared vision of a brighter, more equitable future.
+             Get to know the faces driving our mission and the values that guide our work..</p>
+
+             <p>
+             Thank you for being a part of our journey. Together, we can turn compassion into action and make the 
+             world a better place for all !!!
+             </p>
+      </div>
+    </div>
+  </div>
+  <a href="index.php" class="btn btn-dark">BACK</a>
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+</body>
+</html>
