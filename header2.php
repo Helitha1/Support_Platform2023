84116f08a55a6e80f57603258b43ffd3f932928f
Nikeshana <146467081+Nikeshana@users.noreply.github.com>
2023-10-05 22:33:36 +0530

    dashboard , header

--- header2.php
+++ header2.php
@@ -13,57 +13,66 @@
 </head>
 
 <body>
-<div class="col-12 container-fluid">
-    <div class="row">
+    <div class="col-12 container-fluid">
+        <div class="row">
 
-    
-    <!-- NAVIGATION -->
-    <div class="navigation-container">
 
-        <!-- LOGO -->
-        <div class="brand-logo">
-            <img src="res/logo-sm.png" alt="Migten Logo" class="" />
-        </div>
+            <!-- NAVIGATION -->
+            <div class="navigation-container">
 
-        <!-- NAVIGATION MENUE ITEMS AND OTHER MENU ICONS -->
-        <div class="navigation-bar" id="nav-menu">
-            <!-- MENI TEXTS -->
-            <ul class="navigation-bar-text">
-                <li class="navigation-item"><a href="" class="active">Home</a></li>
-                <li class="navigation-item"><a href="">About</a></li>
-                <li class="navigation-item"><a href="">Contact Us</a></li>
-                <li class="navigation-item"><a href="">Other Services</a></li>
-                <li class="navigation-item"><a href="">Sign In</a></li>
-                <!-- <li class="navigation-item"><a href="">Dashboard</a></li> -->
-            </ul>
-            <!-- MENU LOGOS -->
-            <ul class="navigation-bar-logo">
-                
-                <li class="navigation-item">
-                    <button><i class="fa-solid fa-moon"></i></button>
-                </li>
-                <!-- USER ACCOUNT -->
-                <li class="navigation-item">
-                    <div class="col-12">
-                        <button class="btn btn-sm btn-primary">
-                        <i class="bi bi-arrow-down-circle"></i> Download
-                        </button>
-                    </div>
-                </li>
-            </ul>
-        </div>
+                <!-- LOGO -->
+                <div class="brand-logo">
+                    <img src="res/logo-sm.png" alt="Migten Logo" class="" />
+                </div>
 
-        <!-- TOGGLE BUTTON -->
-        <div class="d-block d-md-none navigation-toggler">
-            <button class="btn btn-primary" onclick="toggleNavigation('nav-menu','navigation-toggler-icon')" id="navigation-toggler">
-                <i class="fa-solid fa-bars" id="navigation-toggler-icon"></i>
-            </button>
+                <!-- NAVIGATION MENUE ITEMS AND OTHER MENU ICONS -->
+                <div class="navigation-bar" id="nav-menu">
+                    <!-- MENI TEXTS -->
+                    <ul class="navigation-bar-text">
+                        <li class="navigation-item"><a href="" class="active">Home</a></li>
+                        <li class="navigation-item"><a href="">About</a></li>
+                        <li class="navigation-item"><a href="">Contact Us</a></li>
+                        <li class="navigation-item"><a href="">Other Services</a></li>
+                        <div class="dropdown">
+                            <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Sign In
+                            </button>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="#">Action</a></li>
+                                <li><a class="dropdown-item" href="#">Another action</a></li>
+                                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                            </ul>
+                        </div>
+                        <!-- <li class="navigation-item"><a href="">Dashboard</a></li> -->
+                    </ul>
+                    <!-- MENU LOGOS -->
+                    <ul class="navigation-bar-logo">
+
+                        <li class="navigation-item">
+                            <button><i class="fa-solid fa-moon"></i></button>
+                        </li>
+                        <!-- USER ACCOUNT -->
+                        <li class="navigation-item">
+                            <div class="col-12">
+                                <button class="btn btn-sm btn-primary">
+                                    <i class="bi bi-arrow-down-circle"></i> Download
+                                </button>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+
+                <!-- TOGGLE BUTTON -->
+                <div class="d-block d-md-none navigation-toggler">
+                    <button class="btn btn-primary" onclick="toggleNavigation('nav-menu','navigation-toggler-icon')" id="navigation-toggler">
+                        <i class="fa-solid fa-bars" id="navigation-toggler-icon"></i>
+                    </button>
+                </div>
+            </div>
         </div>
     </div>
-    </div>
-</div>
-<script src="main.js"></script>
-<script src="res/bootstrap.bundle.js"></script>
+    <script src="main.js"></script>
+    <script src="res/bootstrap.bundle.js"></script>
 </body>
 
 </html>
