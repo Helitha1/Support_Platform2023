0d9d885ab0fc810c17e4056e80776563385afb8d
Yaso <yasogaran@gmail.com>
2023-10-05 13:37:36 +0530

    Update signUpProcess.php

--- server/signUpProcess.php
+++ server/signUpProcess.php
@@ -10,25 +10,25 @@ if (isset($_POST["json"])) {
     if (isset($request_obj->email) && isset($request_obj->fname) && isset($request_obj->lname) && isset($request_obj->password) && isset($request_obj->gender) && isset($request_obj->profession_id)) {
 
 
-        if ($req_object->fname=="") {
+        if ($request_obj->fname=="") {
             $code = 1;
-        } else if (empty($req_object->lname)) {
+        } else if (empty($request_obj->lname)) {
             $code = 2;
-        } else if (strlen($req_object->fname) > 45) {
+        } else if (strlen($request_obj->fname) > 45) {
             $code = 3;
-        } else if (strlen($req_object->lname) > 45) {
+        } else if (strlen($request_obj->lname) > 45) {
             $code = 4;
-        } else if (empty($req_object->email)) {
+        } else if (empty($request_obj->email)) {
             $code = 5;
-        } else if (strlen($req_object->email) >= 100) {
+        } else if (strlen($request_obj->email) >= 100) {
             $code = 6;
-        } else if (!filter_var($ereq_object->mail, FILTER_VALIDATE_EMAIL)) {
+        } else if (!filter_var($erequest_obj->mail, FILTER_VALIDATE_EMAIL)) {
             $code = 7;
-        } else if (empty($req_object->password)) {
+        } else if (empty($request_obj->password)) {
             $code = 8;
-        } else if (strlen($req_object->password) < 5 || strlen($password) > 20) {
+        } else if (strlen($request_obj->password) < 5 || strlen($password) > 20) {
             $code = 9;
-        } else if (empty($req_object->profession_id)) {
+        } else if (empty($request_obj->profession_id)) {
             $code = 10;
         } else {
             $rs = Database::search("SELECT * FROM `users` WHERE `email`='" . $email . "'");
@@ -42,12 +42,12 @@ if (isset($_POST["json"])) {
                 $d->setTimezone($tz);
                 $date = $d->format("Y-m-d H:i:s");
 
-                $password_hash = password_hash($req_object->password, 0);
+                $password_hash = password_hash($request_obj->password, 0);
 
 
                 Database::iud("INSERT INTO `users` 
                 (`fname`,`lname`,`email`,`password`,`gender_id`,`date_register`,`profession_id`) VALUES 
-                ('" . $req_object->fname . "','" . $req_object->lname . "','" . $req_object->email . "','" . $password_hash . "','" . $req_object->gender . "','" . $req_object->date . "','" . $req_object->profession_id . "')");
+                ('" . $request_obj->fname . "','" . $request_obj->lname . "','" . $request_obj->email . "','" . $password_hash . "','" . $request_obj->gender . "','" . $request_obj->date . "','" . $request_obj->profession_id . "')");
 
                 $code =  100;
             }
