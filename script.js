a460fd3b5c5940304248d05e6c344f6ca068d917
Yaso <yasogaran@gmail.com>
2023-10-06 06:41:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -110,7 +110,7 @@ forgotPassword = () => {
         bm = new bootstrap.Modal(m);
         bm.show();
       } else {
-        alert(obj.code);
+        alert(showError(obj.code));
       }
     })
     .catch((error) => console.log(error));
@@ -141,7 +141,7 @@ resetPassword = () => {
         if (obj.code == 100) {
           window.location.reload();
         } else {
-          showError(obj.code);
+          alert(showError(obj.code));
         }
       })
       .catch((error) => console.log(error));
@@ -158,7 +158,7 @@ loadProject = (id) => {
       projectObject = obj.data;
       fillData();
     })
-    .catch(err => alert(err))
+    .catch(err => alert(showError(err)))
 
   fillData = () => {
     _('project-title').innerHTML = projectObject.title;
@@ -293,47 +293,47 @@ function addNewProject() {
 
 
 
-showError=(code)=>{
+showError = (code) => {
   switch (code) {
-      case 99:
-          return('REQUEST JSON ERROR')
-      case 1:
-          return('First Name is null ')
-      case 2:
-          return('Last Name is null')
-      case 3:
-          return('first name must be less that 45 characters ')
-      case 4:
-          return('last name must be less that 45 characters ')
-      case 5:
-          return('email is null')
-      case 6:
-          return('email must be less that 100 characters ')
-      case 7:
-          return('invalid email')
-      case 8:
-          return('password is null')
-      case 9:
-          return('password must be more that 5 and less that 20 characters')
-      case 10:
-          return('profession not selected')
-      case 11:
-          return('this email alredy used');
-      case 12:
-          return('user not found');
-      case 13:
-          return('Invalid password, try again!');
-      case 14:
-          return('re enter your password!');
-      case 15:
-          return('password dosent match!');
-      case 16:
-          return('verification code empty!');
-      case 17:
-          return('invalid email or verifiacation code!');
-      case 18:
-          return('Verification Code sending Failed');
-      default:
-          return(code)
+    case 99:
+      return ('REQUEST JSON ERROR')
+    case 1:
+      return ('First Name is null ')
+    case 2:
+      return ('Last Name is null')
+    case 3:
+      return ('first name must be less that 45 characters ')
+    case 4:
+      return ('last name must be less that 45 characters ')
+    case 5:
+      return ('email is null')
+    case 6:
+      return ('email must be less that 100 characters ')
+    case 7:
+      return ('invalid email')
+    case 8:
+      return ('password is null')
+    case 9:
+      return ('password must be more that 5 and less that 20 characters')
+    case 10:
+      return ('profession not selected')
+    case 11:
+      return ('this email alredy used');
+    case 12:
+      return ('user not found');
+    case 13:
+      return ('Invalid password, try again!');
+    case 14:
+      return ('re enter your password!');
+    case 15:
+      return ('password dosent match!');
+    case 16:
+      return ('verification code empty!');
+    case 17:
+      return ('invalid email or verifiacation code!');
+    case 18:
+      return ('Verification Code sending Failed');
+    default:
+      return (code)
   }
 }
